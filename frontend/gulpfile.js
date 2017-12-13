4008ba351639593932588283c8d915434b84b830
error418 <error418@users.noreply.github.com>
2017-12-13 13:37:38 +0100

    frontend build

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,9 +1,25 @@
+var gulp = require('gulp');
 var concat = require('gulp-concat');
+var ngAnnotate = require('gulp-ng-annotate');
 
-var srcJs = ['./src/js/*.js']
+var srcJs = ['src/js/app.js', 'src/js/**/*.js'];
+var assets = ['src/**/*.html'];
 
-gulp.task('scripts', function() {
+gulp.task('build', function() {
  return gulp.src(srcJs)
-   .pipe(concat('all.js'))
-   .pipe(gulp.dest('./dist/'));
+    .pipe(ngAnnotate())
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./dist/'));
 });
+
+gulp.task("assets", function() {
+    return gulp.src(assets)
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task("watch", function () {
+    return gulp.watch(srcJs.concat(assets), ['build', 'assets']);
+});
+
+gulp.task("default", ["build", "assets"]);
+
--- frontend/index.js
+++ frontend/index.js
@@ -3,8 +3,11 @@ var app = express()
 
 app.use('/css/', express.static('node_modules/bootstrap/dist/css'));
 app.use('/js/', express.static('node_modules/bootstrap/dist/js'));
+app.use('/js/', express.static('node_modules/angular'));
+app.use('/js/', express.static('node_modules/angular-resource'));
+app.use('/js/', express.static('node_modules/angular-route'));
 app.use('/fonts/', express.static('node_modules/bootstrap/dist/fonts'));
 
-app.use('/', express.static('src'));
+app.use('/', express.static('dist'));
  
 app.listen(3000)
