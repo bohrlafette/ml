bfa1a79597f1cde2838251078ebdf6ec99d8300c
Peter Pytel <peter.pytel@gmx.net>
2017-12-13 10:17:59 +0100

    Erweiterung der Funktionen

--- service/testdaten.py
+++ service/testdaten.py
@@ -1,13 +1,12 @@
 import random
-
-i = 1
+import numpy as np
 
 NO = 0
 YES = 1
 
-def user(name, gender, numChildren, ownsHouse, bornBefore1990, bornBefore1998, numCats, numDogs, numHorses):
+def user(name, gender, numChildren, ownsHouse, yearBorn, numCats, numDogs, numHorses):
     #print "adding user " + name
-    return [gender, numChildren, ownsHouse, bornBefore1990, bornBefore1998, numCats, numDogs, numHorses]
+    return [gender, numChildren, ownsHouse, yearBorn, numCats, numDogs, numHorses]
 
 def item(name, secureFamily, secureProperty, retirement, health, animals):
     #print "adding item " + name
@@ -19,10 +18,9 @@ def interaction(haft, foerder, zahn, pferd, hunde, hausrat, kfz):
 def createYoungAgedSingle():
     return (user('YoungAgedSingle', 
             random.randint(0,1), 
-            0, 
-            0, 
-            0,
-            0, 
+            int(round(random.uniform(0.0,0.65))), 
+            int(round(random.uniform(0.0,0.65))), 
+            random.randint(1997, 2001),
             int(round(random.uniform(0.0,0.65))), 
             random.randint(0,1), 
             int(round(random.uniform(0.0,0.60)))))
@@ -30,10 +28,9 @@ def createYoungAgedSingle():
 def createMidAgedFamilyMember():
     return (user('MidAgedFamilyMember', 
             random.randint(0,1), 
-            1, 
+            random.randint(0,4),
             random.randint(0,1), 
-            1,
-            1,
+            random.randint(1987, 1995),
             int(round(random.uniform(0.2,1.0))), 
             int(round(random.uniform(0.2,1.0))), 
             random.randint(0,1)))
@@ -41,22 +38,21 @@ def createMidAgedFamilyMember():
 def createMidAgedSingle():
     return (user('MidAgedSingle', 
             random.randint(0,1), 
-            0, 
             int(round(random.uniform(0.0,0.65))),
-            1,
-            1,
+            int(round(random.uniform(0.0,0.65))),
+            random.randint(1977, 1990),
             int(round(random.uniform(0.2,1.0))), 
             int(round(random.uniform(0.2,1.0))),
             int(round(random.uniform(0.0,0.65)))))
 
-def generateInteraction(numChildren, ownsHouse, bornBefore1990, bornBefore1998, numCats, numDogs, numHorses):
+def generateInteraction(numChildren, ownsHouse, yearBorn, numCats, numDogs, numHorses):
 
-    haft = int(round(random.uniform(-0.6,1.0)))
-    zahn = int(round(random.uniform(0.1,1.0)))
+    haft = int(round(random.uniform(0.2,1.0)))
+    zahn = int(round(random.uniform(-0.6,1.0)))
 
     foerder = 0
-    pferd = 0
-    hunde = 0
+    pferd = -1
+    hunde = -1
     hausrat = 0
     kfz = 0
 
@@ -69,15 +65,15 @@ def generateInteraction(numChildren, ownsHouse, bornBefore1990, bornBefore1998, 
     if ownsHouse > 0:
         hausrat= int(round(random.uniform(0.4,1.0)))
 
-    if bornBefore1990 == 1:
+    if yearBorn <= 1992:
         foerder = int(round(random.uniform(0.3,1.0)))
     else:
-        foerder = int(round(random.uniform(0,0.6)))
+        foerder = int(round(random.uniform(-0.7,0.6)))
 
-    if bornBefore1998 == 1:
-        kfz = int(round(random.uniform(0,0.7)))
+    if yearBorn <= 1997:
+        kfz = int(round(random.uniform(-0.7,0.7)))
     else:
-        kfz = -int(round(random.uniform(0.3,1.0)))
+        kfz = int(round(random.uniform(0.3,1.0)))
     
     return interaction(haft, foerder, zahn, pferd, hunde, hausrat, kfz)
 
@@ -91,7 +87,7 @@ items = [
     item("kfz", NO, YES, NO, NO, NO)
 ]
 
-def createTestdata(typ):
+def createSingleTestdata(typ):
     if typ == 0:
         testUser = createYoungAgedSingle()
     elif typ == 1:
@@ -105,19 +101,31 @@ def createTestdata(typ):
                                         testUser[3],
                                         testUser[4],
                                         testUser[5],
-                                        testUser[6],
-                                        testUser[7])
-
+                                        testUser[6])
+                                        
     return testUser, generatedInteraction
 
-while i <= 10:
+def createMultipleTestdata(count):
+    i = 1
+    initialUser, initialInteraction = createSingleTestdata(random.randint(0,2),)
+    users = [initialUser]
+    interactions = [initialInteraction]
 
-    print("Start")
+    while i < count:
 
-    testUser, generatedInteraction = createTestdata(random.randint(0,2),)
+        testUser, generatedInteraction = createSingleTestdata(random.randint(0,2),)
 
-    print(testUser)
-    print(generatedInteraction)
-   
-    i = i + 1
+        users = np.append(users, [testUser], axis=0)
+        interactions = np.append(interactions, [generatedInteraction], axis=0)
 
+        i = i + 1
+    return users, interactions
+
+
+users, interactions = createMultipleTestdata(20)
+
+print("[gender, numChildren, ownsHouse, yearBorn, numCats, numDogs, numHorses]")
+print(users)
+
+print("[haft, foerder, zahn, pferd, hunde, hausrat, kfz]")
+print(interactions)
