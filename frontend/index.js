e1cb5988704cd56196e864aad92c12ad66f4498e
error418 <error418@users.noreply.github.com>
2018-01-09 12:14:29 +0100

    make things more appealing to the eye

--- frontend/index.js
+++ frontend/index.js
@@ -3,6 +3,7 @@ var proxy = require('http-proxy-middleware');
 var app = express()
 
 app.use('/css/', express.static('node_modules/bootstrap/dist/css'));
+app.use('/css/', express.static('node_modules/font-awesome/css'));
 app.use('/js/', express.static('node_modules/bootstrap/dist/js'));
 
 app.use('/lib/', express.static('node_modules/angular-ui-bootstrap/dist'));
@@ -13,6 +14,7 @@ app.use('/js/', express.static('node_modules/angular-animate'));
 app.use('/js/', express.static('node_modules/angular-resource'));
 app.use('/js/', express.static('node_modules/angular-route'));
 app.use('/fonts/', express.static('node_modules/bootstrap/dist/fonts'));
+app.use('/fonts/', express.static('node_modules/font-awesome/fonts'));
 
 app.use('/', express.static('dist'));
 
